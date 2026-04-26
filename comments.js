5fd21a3ccb3957e53affe4b682d70f62b16c9cc6
Mohammed Rihan S <mohammedrihan.s2025cse@sece.ac.in>
2026-04-26 09:02:30 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,13 @@
+//Create web server
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
